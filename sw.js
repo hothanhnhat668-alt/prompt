bc009bbcff00bf198ad40da85a36601083faf7bb
hothanhnhat668-alt <hothanhnhat668@gmail.com>
2025-10-27 20:34:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
 const CACHE_NAME = 'aoligei-prompt-cache-v1';
-const ASSETS = ['./','./index.html','./manifest.json'];
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
